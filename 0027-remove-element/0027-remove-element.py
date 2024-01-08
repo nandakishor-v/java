6782a2c75eec9c92fb2e7265113fa833bdac81ea
Nandakishor.V <78819133+nandakishor-v@users.noreply.github.com>
2024-01-08 11:16:37 +0530

    Time: 7 ms (99.38%), Space: 13 MB (98.37%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        i = 0
+        while i < len(nums):
+            if nums[i] == val:
+                nums.pop(i)
+            else:
+                i += 1
+
+        
